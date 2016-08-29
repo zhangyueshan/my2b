7fe5e6c8e467d30cf5cda06bdf9ed904e930bddc
zhangyueshan <1067657474@qq.com>
2016-08-29 16:24:05 +0800

    first

---
+++ src/main/java/wings/my2b/io/Packet.java
@@ -0,0 +1,7 @@
+package wings.my2b.io;
+
+/**
+ * Created by InThEnd on 2016/8/29.
+ */
+public class Packet {
+}
---
+++ src/main/java/wings/my2b/io/ToBeMain.java
@@ -0,0 +1,17 @@
+package wings.my2b.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+/**
+ * Created by InThEnd on 2016/8/29.
+ * ToBeMain
+ */
+public class ToBeMain {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("192.168.2.9", 3306);
+        InputStream is = socket.getInputStream();
+    }
+}
